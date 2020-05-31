f234349049f134ba4616d0441432fde0d41b9ad9
Dimitris Christodoulou <demetrischr1998@gmail.com>
2020-05-31 22:53:49 +0100

    Update web app files

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from flask import render_template, send_from_directory
+from app import app
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Home')
+
+
+@app.route('/map')
+def map():
+    return render_template('map.html', title='Map')
+
+
+@app.route('/data')
+def data():
+    return render_template('data.html', title='Data')
